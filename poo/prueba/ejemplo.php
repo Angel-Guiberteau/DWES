a8b5754f356abe49ee12161eaf5d1b8e4dc8bf58
aguiberteauEVG <147706971+Angel-Guiberteau@users.noreply.github.com>
2024-10-09 12:46:11 +0200

    Add files via upload

---
+++ poo/prueba/ejemplo.php
@@ -0,0 +1,10 @@
+<?php
+    class Ejemplo{
+        public int $dato = 10;
+
+        function metodo1(){
+            $this->dato = 10;
+        }
+    }
+
+?>
---
+++ poo/prueba/usar_clase.php
@@ -0,0 +1,9 @@
+<?php
+
+    require_once 'ejemplo.php';
+
+    $objEjemplo = new Ejemplo();
+    $obbjEjemplo->metodo1();
+    echo 'El dato es: '.$objEjemplo->dato;
+
+?>
